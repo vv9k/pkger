ddfe343eaca93c80b665af0999a40807a4aeb477
wojciech.kepka <wojtek.kepka@protonmail.com>
2020-01-02 14:14:56 +0100

    make it start nicely

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use failure::Error;
+use log::*;
 use pkger::Pkger;
 use structopt::StructOpt;
 
@@ -10,6 +11,8 @@ struct Opt {
     /// URL to dockers api
     #[structopt(short, long)]
     docker: String,
+    /// Recipes to build
+    recipes: Vec<String>,
     /// Path to config file (default - "./conf.toml")
     #[structopt(short, long)]
     config: Option<String>,
@@ -19,11 +22,14 @@ struct Opt {
 async fn main() -> Result<(), Error> {
     pretty_env_logger::init();
     let opts = Opt::from_args();
+    trace!("{:?}", opts);
     let cfg = opts.config.unwrap_or(DEFAULT_CONF_FILE.to_string());
+    let pkger = Pkger::new(&opts.docker, &cfg)?;
+    trace!("{:?}", pkger);
 
-    let pkgr = Pkger::new(&opts.docker, &cfg)?;
-
-    pkgr.build_recipe("curl").await?;
+    for recipe in opts.recipes.iter() {
+        pkger.build_recipe(&recipe).await.unwrap()
+    }
 
     Ok(())
 }
