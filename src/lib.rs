d0646539ed0fe585fcb08c65f6cc2f2151d1ae2f
wojciechkepka <wojtek.kepka@protonmail.com>
2020-01-06 10:36:24 +0100

    move utility stuff to util.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@
 extern crate failure;
 extern crate tar;
 mod recipe;
+mod util;
 use self::recipe::*;
+use self::util::*;
 use chrono::prelude::Local;
 use failure::Error;
 use hyper::{Body, Uri};
@@ -962,32 +964,6 @@ impl Pkger {
             )),
         }
     }
-}
-
-fn find_penultimate_ancestor<P: AsRef<Path>>(path: P) -> PathBuf {
-    trace!("finding parent of {}", path.as_ref().display());
-    let mut ancestors = path.as_ref().ancestors();
-    loop {
-        match ancestors.next() {
-            Some(ancestor) => {
-                if ancestors.next() == Some(Path::new("")) {
-                    trace!("found {}", ancestor.display());
-                    return ancestor.to_path_buf();
-                }
-            }
-            None => return PathBuf::from(""),
-        }
-    }
-}
-
-fn should_include<P: AsRef<Path>>(path: P, excludes: &[String]) -> bool {
-    trace!("checking if {} should be included", path.as_ref().display());
-    for e in excludes {
-        if path.as_ref().starts_with(e) {
-            return false;
-        }
-    }
-    true
 }
 
 // # TODO
