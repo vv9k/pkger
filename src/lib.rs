46cc5af13bf8800ccf0ac9fbc1b616ce19399748
Wojciech Kępka <wojciech@wkepka.dev>
2020-01-06 10:36:41 +0100

    move image related stuff to image.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 #[macro_use]
 extern crate failure;
 extern crate tar;
+mod image;
 mod recipe;
 mod util;
+use self::image::*;
 use self::recipe::*;
 use self::util::*;
 use chrono::prelude::Local;
@@ -72,103 +74,6 @@ pub struct Config {
     images_dir: String,
     recipes_dir: String,
     output_dir: String,
-}
-
-#[derive(Debug)]
-struct Image {
-    name: String,
-    path: PathBuf,
-    has_dockerfile: bool,
-}
-impl Image {
-    fn new(entry: DirEntry) -> Image {
-        let path = entry.path();
-        let has_dockerfile = Image::has_dockerfile(path.clone());
-        Image {
-            name: entry.file_name().into_string().unwrap_or_default(),
-            path,
-            has_dockerfile,
-        }
-    }
-    fn has_dockerfile(mut p: PathBuf) -> bool {
-        p.push("Dockerfile");
-        p.as_path().exists()
-    }
-    fn should_be_rebuilt(&self) -> Result<bool, Error> {
-        trace!("checking if image {} should be rebuilt", &self.name);
-        let state = ImageState::load(DEFAULT_STATE_FILE)?;
-        if let Some(prvs_bld_time) = state.images.get(&self.name) {
-            match fs::metadata(self.path.as_path()) {
-                Ok(metadata) => match metadata.modified() {
-                    Ok(mod_time) => {
-                        if mod_time > prvs_bld_time.1 {
-                            trace!("image directory was modified since last build so marking for rebuild");
-                            return Ok(true);
-                        } else {
-                            return Ok(false);
-                        }
-                    }
-                    Err(e) => error!(
-                        "failed to retrive modification date of {} - {}",
-                        self.path.as_path().display(),
-                        e
-                    ),
-                },
-                Err(e) => error!(
-                    "failed to read metadata of {} - {}",
-                    self.path.as_path().display(),
-                    e
-                ),
-            }
-        }
-        Ok(true)
-    }
-}
-type Images = HashMap<String, Image>;
-
-#[derive(Deserialize, Debug, Default, Serialize)]
-struct ImageState {
-    images: HashMap<String, (String, SystemTime)>,
-    #[serde(skip)]
-    statef: String,
-}
-impl ImageState {
-    fn load<P: AsRef<Path>>(statef: P) -> Result<Self, Error> {
-        let path = format!("{}", statef.as_ref().display());
-        if !statef.as_ref().exists() {
-            trace!("no previous state file, creating new in {}", &path);
-            if let Err(e) = File::create(statef.as_ref()) {
-                return Err(format_err!(
-                    "failed to create state file in {} - {}",
-                    &path,
-                    e
-                ));
-            }
-            return Ok(ImageState {
-                images: HashMap::new(),
-                statef: path,
-            });
-        }
-        trace!("loading image state file from {}", &path);
-        let contents = fs::read(statef.as_ref())?;
-        let mut s: ImageState = serde_json::from_slice(&contents)?;
-        trace!("{:?}", s);
-        s.statef = path;
-        Ok(s)
-    }
-    fn update(&mut self, image: &str, current_tag: &str) {
-        trace!("updating build time of {}", image);
-        self.images.insert(
-            image.to_string(),
-            (current_tag.to_string(), SystemTime::now()),
-        );
-    }
-    fn save(&self) -> Result<(), Error> {
-        trace!("saving images state to {}", &self.statef);
-        trace!("{:#?}", &self);
-        fs::write(&self.statef, serde_json::to_vec(&self)?).unwrap();
-        Ok(())
-    }
 }
 
 #[derive(Debug)]
