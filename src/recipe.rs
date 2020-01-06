12475f2241acc8f633f995ba2efbe8a9c2019419
wojciechkepka <wojtek.kepka@protonmail.com>
2020-01-06 11:09:25 +0100

    move generate_deb_control to recipe.rs

--- src/recipe.rs
+++ src/recipe.rs
@@ -49,3 +49,84 @@ pub struct Install {
     pub steps: Vec<String>,
     pub destdir: String,
 }
+
+// # TODO
+// Find a nicer way to generate this
+pub fn generate_deb_control(info: &Info) -> String {
+    let arch = match &info.arch[..] {
+        "x86_64" => "amd64",
+        // #TODO
+        _ => "unimplemented",
+    };
+    trace!("generating control file");
+    let mut control = format!(
+        "Package: {}
+Version: {}-{}
+Architecture: {}
+",
+        &info.name, &info.version, &info.revision, &arch
+    );
+    control.push_str("Section: ");
+    match &info.section {
+        Some(section) => control.push_str(section),
+        None => control.push_str("base"),
+    }
+    control.push_str("\nPriority: ");
+    match &info.priority {
+        Some(priority) => control.push_str(priority),
+        None => control.push_str("optional"),
+    }
+    control.push('\n');
+
+    if let Some(dependencies) = &info.depends {
+        control.push_str("Depends: ");
+        let mut deps = String::new();
+        for d in dependencies {
+            trace!("adding dependency {}", d);
+            deps.push_str(&format!("{}, ", d));
+        }
+        control.push_str(deps.trim_end_matches(", "));
+        control.push('\n');
+    }
+    if let Some(conflicts) = &info.conflicts {
+        control.push_str("Conflicts: ");
+        let mut confs = String::new();
+        for c in conflicts {
+            trace!("adding conflict {}", c);
+            confs.push_str(&format!("{}, ", c));
+        }
+        control.push_str(confs.trim_end_matches(", "));
+        control.push('\n');
+    }
+    if let Some(obsoletes) = &info.obsoletes {
+        control.push_str("Breaks: ");
+        let mut obs = String::new();
+        for o in obsoletes {
+            trace!("adding obsolete {}", o);
+            obs.push_str(&format!("{}, ", o));
+        }
+        control.push_str(obs.trim_end_matches(", "));
+        control.push('\n');
+    }
+    if let Some(provides) = &info.provides {
+        control.push_str("Provides: ");
+        let mut prvds = String::new();
+        for p in provides {
+            trace!("adding provide {}", p);
+            prvds.push_str(&format!("{}, ", p));
+        }
+        control.push_str(prvds.trim_end_matches(", "));
+        control.push('\n');
+    }
+
+    control.push_str("Maintainer: ");
+    match &info.maintainer {
+        Some(maintainer) => control.push_str(maintainer),
+        None => control.push_str("null <null@email.com>"),
+    }
+
+    control.push_str(&format!("\nDescription: {}\n", &info.description));
+
+    trace!("{}", &control);
+    control
+}
