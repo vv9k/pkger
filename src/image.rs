d4e7dd2ebc002fca2860762fbd0cb2c1413448e5
wojciechkepka <wojtek.kepka@protonmail.com>
2021-04-09 16:17:49 +0200

    Add more distribution support

--- src/image.rs
+++ src/image.rs
@@ -140,7 +140,7 @@ impl ImageState {
         Ok(ImageState {
             id: id.to_string(),
             image: image.to_string(),
-            os: Os::from(os_name, version)?,
+            os: Os::from(os_name, version),
             tag: tag.to_string(),
             timestamp: *timestamp,
         })
@@ -216,50 +216,62 @@ impl ImagesState {
 // enum holding version of os
 #[derive(Debug, Deserialize, Clone, Serialize)]
 pub enum Os {
-    Debian(String),
-    Redhat(String),
     Arch(String),
+    Centos(String),
+    Debian(String),
+    Fedora(String),
+    Redhat(String),
+    Ubuntu(String),
     Unknown,
 }
 
 impl AsRef<str> for Os {
     fn as_ref(&self) -> &str {
         match self {
-            Os::Debian(_) => "debian",
             Os::Arch(_) => "arch",
+            Os::Centos(_) => "centos",
+            Os::Debian(_) => "debian",
+            Os::Fedora(_) => "fedora",
             Os::Redhat(_) => "redhat",
+            Os::Ubuntu(_) => "ubuntu",
             Os::Unknown => "unknown",
         }
     }
 }
 
 impl Os {
-    pub fn from(os: Option<String>, version: Option<String>) -> Result<Os, Error> {
+    pub fn from(os: Option<String>, version: Option<String>) -> Os {
         if let Some(os) = os {
             let version = version.unwrap_or_default();
             match &os[..] {
-                "ubuntu" | "debian" => Ok(Os::Debian(version)),
-                "centos" | "redhat" | "fedora" => Ok(Os::Redhat(version)),
-                os => Err(format_err!("unknown os {}", os)),
+                "arch" => Os::Arch(version),
+                "centos" => Os::Centos(version),
+                "debian" => Os::Debian(version),
+                "fedora" => Os::Fedora(version),
+                "redhat" => Os::Redhat(version),
+                _ => Os::Unknown,
             }
         } else {
-            Ok(Os::Unknown)
+            Os::Unknown
         }
     }
     pub fn os_ver(&self) -> &str {
         match self {
-            Os::Debian(v) => v.as_str(),
-            Os::Redhat(v) => v.as_str(),
             Os::Arch(v) => v.as_str(),
+            Os::Centos(v) => v.as_str(),
+            Os::Debian(v) => v.as_str(),
+            Os::Fedora(v) => v.as_str(),
+            Os::Redhat(v) => v.as_str(),
+            Os::Ubuntu(v) => v.as_str(),
             Os::Unknown => "",
         }
     }
     pub fn package_manager(&self) -> &str {
         match self {
-            Os::Debian(_) => "apt-get",
-            Os::Redhat(v) if v == "8" => "dnf",
-            Os::Redhat(_) => "yum",
             Os::Arch(_) => "pacman",
+            Os::Debian(_) | Os::Ubuntu(_) => "apt-get",
+            Os::Redhat(v) | Os::Centos(v) | Os::Fedora(v) if v == "8" => "dnf",
+            Os::Redhat(_) | Os::Centos(_) | Os::Fedora(_) => "yum",
             Os::Unknown => "",
         }
     }
