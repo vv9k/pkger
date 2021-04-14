f94fc0c38230b4f4856eab2db5b22f3b0b4fdc98
wojciechkepka <wojtek.kepka@protonmail.com>
2021-04-14 20:33:12 +0200

    Add flate2 dependency, add archive util methods, cleanup util

--- src/util.rs
+++ src/util.rs
@@ -1,37 +1,58 @@
 //! Helper functions that don't fit anywhere else
-#![allow(dead_code)]
 
-use std::path::{Path, PathBuf};
+use crate::Result;
 
-#[macro_export]
-macro_rules! map_return {
-    ($f:expr, $e:expr) => {
-        match $f {
-            Ok(d) => d,
-            Err(e) => return Err(anyhow!("{} - {}", $e, e)),
-        }
-    };
-}
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::path::Path;
+use tracing::{info_span, trace};
 
-pub fn find_penultimate_ancestor<P: AsRef<Path>>(path: P) -> PathBuf {
-    let mut ancestors = path.as_ref().ancestors();
-    loop {
-        match ancestors.next() {
-            Some(ancestor) => {
-                if ancestors.next() == Some(Path::new("")) {
-                    return ancestor.to_path_buf();
-                }
-            }
-            None => return PathBuf::from(""),
-        }
-    }
-}
+pub fn unpack_archive<T: io::Read, P: AsRef<Path>>(
+    archive: &mut tar::Archive<T>,
+    output_dir: P,
+) -> Result<()> {
+    let span = info_span!("unpack-archive");
+    let _enter = span.enter();
 
-pub fn should_include<P: AsRef<Path>>(path: P, excludes: &[String]) -> bool {
-    for e in excludes {
-        if path.as_ref().starts_with(e) {
-            return false;
+    let output_dir = output_dir.as_ref();
+
+    for entry in archive.entries()? {
+        let mut entry = entry?;
+        if let tar::EntryType::Regular = entry.header().entry_type() {
+            let path = entry.header().path()?.to_path_buf();
+            trace!(parent: &span, entry = %path.display(), to = %output_dir.display(), "unpacking");
+            let name = path.file_name().unwrap_or_default();
+
+            entry.unpack(output_dir.join(name))?;
         }
     }
-    true
+
+    Ok(())
+}
+
+pub fn save_tar_gz<T: io::Read>(
+    archive: tar::Archive<T>,
+    name: &str,
+    output_dir: &Path,
+) -> Result<()> {
+    let path = output_dir.join(name);
+
+    let span = info_span!("unpack-archive", path = %path.display());
+    let _enter = span.enter();
+
+    trace!("creating a gzipped tarball");
+    let f = File::create(path.as_path())?;
+    let mut e = GzEncoder::new(f, Compression::default());
+    let mut archive = archive.into_inner();
+    let mut bytes = Vec::new();
+    archive.read_to_end(&mut bytes)?;
+
+    e.write_all(&bytes)?;
+
+    e.finish()?;
+
+    Ok(())
 }
