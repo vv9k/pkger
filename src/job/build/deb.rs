4130e53a015e63712221846b59f353e692c619ac
Wojciech Kępka <wojtek.kepka@protonmail.com>
2021-04-20 22:04:08 +0200

    Cleanup tracing spans

--- src/job/build/deb.rs
+++ src/job/build/deb.rs
@@ -24,69 +24,68 @@ impl<'job> BuildContainerCtx<'job> {
         let package_name = [&name, ".", &arch].join("");
 
         let span = info_span!("DEB", package = %package_name);
+        let cloned_span = span.clone();
+        async move {
+            info!("building DEB package");
 
-        info!(parent: &span, "building DEB package");
+            let debbld_dir = PathBuf::from("/root/debbuild");
+            let tmp_dir = debbld_dir.join("tmp");
+            let base_dir = debbld_dir.join(&name);
+            let deb_dir = base_dir.join("DEBIAN");
+            let dirs = [deb_dir.as_path(), tmp_dir.as_path()];
 
-        let debbld_dir = PathBuf::from("/root/debbuild");
-        let tmp_dir = debbld_dir.join("tmp");
-        let base_dir = debbld_dir.join(&name);
-        let deb_dir = base_dir.join("DEBIAN");
-        let dirs = [deb_dir.as_path(), tmp_dir.as_path()];
+            self.create_dirs(&dirs[..]).await?;
 
-        self.create_dirs(&dirs[..]).instrument(span.clone()).await?;
+            let control = self.recipe.as_deb_control(&image_state.image).render();
+            debug!(control = %control);
 
-        let control = self.recipe.as_deb_control(&image_state.image).render();
-        debug!(parent: &span, control = %control);
+            let entries = vec![("./control", control.as_bytes())];
+            let control_tar = cloned_span.in_scope(|| create_tar_archive(entries.into_iter()))?;
+            let control_tar_path = tmp_dir.join([&name, "-control.tar"].join(""));
 
-        let entries = vec![("./control", control.as_bytes())];
-        let control_tar = span.in_scope(|| create_tar_archive(entries.into_iter()))?;
-        let control_tar_path = tmp_dir.join([&name, "-control.tar"].join(""));
+            trace!("copy control archive to container");
+            self.container
+                .inner()
+                .copy_file_into(control_tar_path.as_path(), &control_tar)
+                .await?;
 
-        trace!(parent: &span, "copy control archive to container");
-        self.container
-            .inner()
-            .copy_file_into(control_tar_path.as_path(), &control_tar)
-            .instrument(span.clone())
+            trace!("extract control archive");
+            self.checked_exec(&format!(
+                "tar -xvf {} -C {}",
+                control_tar_path.display(),
+                deb_dir.display(),
+            ))
             .await?;
 
-        trace!(parent: &span, "extract control archive");
-        self.checked_exec(&format!(
-            "tar -xvf {} -C {}",
-            control_tar_path.display(),
-            deb_dir.display(),
-        ))
-        .instrument(span.clone())
-        .await?;
+            trace!("copy source files to build dir");
+            self.checked_exec(&format!(
+                "cd {} && cp -rv . {}",
+                self.container_out_dir.display(),
+                base_dir.display()
+            ))
+            .await?;
 
-        trace!(parent: &span, "copy source files to build dir");
-        self.checked_exec(&format!(
-            "cd {} && cp -rv . {}",
-            self.container_out_dir.display(),
-            base_dir.display()
-        ))
-        .instrument(span.clone())
-        .await?;
+            let dpkg_deb_opts = if image_state.os.os_ver().parse::<u8>().unwrap_or_default() < 10 {
+                "--build"
+            } else {
+                "--build --root-owner-group"
+            };
 
-        let dpkg_deb_opts = if image_state.os.os_ver().parse::<u8>().unwrap_or_default() < 10 {
-            "--build"
-        } else {
-            "--build --root-owner-group"
-        };
+            self.checked_exec(&format!(
+                "dpkg-deb {} {}",
+                dpkg_deb_opts,
+                base_dir.display()
+            ))
+            .await?;
 
-        self.checked_exec(&format!(
-            "dpkg-deb {} {}",
-            dpkg_deb_opts,
-            base_dir.display()
-        ))
-        .instrument(span.clone())
-        .await?;
+            let deb_name = [&name, ".deb"].join("");
 
-        let deb_name = [&name, ".deb"].join("");
-
-        self.container
-            .download_files(debbld_dir.join(&deb_name).as_path(), output_dir)
-            .instrument(span)
-            .await
-            .map(|_| output_dir.join(deb_name))
+            self.container
+                .download_files(debbld_dir.join(&deb_name).as_path(), output_dir)
+                .await
+                .map(|_| output_dir.join(deb_name))
+        }
+        .instrument(span)
+        .await
     }
 }
