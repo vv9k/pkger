8fab34e7f248d73771622cf1b4017bf13e33cc55
wojciechkepka <wojtek.kepka@protonmail.com>
2021-04-22 15:57:28 +0200

    Split up exec-scripts

--- src/job/build/scripts.rs
+++ src/job/build/scripts.rs
@@ -1,4 +1,5 @@
 use crate::job::build::BuildContainerCtx;
+use crate::recipe::{BuildScript, ConfigureScript, InstallScript};
 use crate::Result;
 
 use tracing::{info, info_span, trace, Instrument};
@@ -8,91 +9,112 @@ impl<'job> BuildContainerCtx<'job> {
         let span = info_span!("exec-scripts");
         async move {
             if let Some(config_script) = &self.recipe.configure_script {
-                let script_span = info_span!("configure");
-                info!(parent: &script_span, "executing configure scripts");
-                let working_dir = if let Some(dir) = &config_script.working_dir {
-                    Some(dir.as_path())
-                } else {
-                    None
-                };
-                let shell = if let Some(shell) = &config_script.shell {
-                    Some(shell.as_str())
-                } else {
-                    None
-                };
-                for cmd in &config_script.steps {
-                    if !cmd.images.is_empty() {
-                        trace!(parent: &script_span, images = ?cmd.images, "only execute on");
-                        if !cmd.images.contains(&self.image.name) {
-                            trace!(parent: &script_span, image = %self.image.name, "not found, skipping");
-                            continue;
-                        }
-                    }
-                    let out = self.checked_exec(&cmd.cmd, working_dir, shell).instrument(script_span.clone()).await?;
-
-                    if out.exit_code != 0 {
-                        return Err(anyhow!(
-                            "command `{}` failed with exit code {}\nError:\n{}",
-                            &cmd.cmd,
-                            out.exit_code,
-                            out.stderr.join("\n")
-                        ));
-                    }
-                }
+                self.run_configure(&config_script).await?;
             } else {
                 info!("no configure steps to run");
             }
 
-            let script_span = info_span!("build");
-            let working_dir = if let Some(dir) = &self.recipe.build_script.working_dir {
+            self.run_build(&self.recipe.build_script).await?;
+
+            if let Some(install_script) = &self.recipe.install_script {
+                self.run_install(&install_script).await?;
+            } else {
+                info!("no install steps to run");
+            }
+
+            Ok(())
+        }
+        .instrument(span)
+        .await
+    }
+
+    async fn run_configure(&self, config_script: &ConfigureScript) -> Result<()> {
+        let span = info_span!("configure");
+        async move {
+            info!("executing configure scripts");
+            let working_dir = if let Some(dir) = &config_script.working_dir {
                 Some(dir.as_path())
             } else {
-                Some(self.container_bld_dir)
+                None
             };
-            let shell = if let Some(shell) = &self.recipe.build_script.shell {
+            let shell = if let Some(shell) = &config_script.shell {
                 Some(shell.as_str())
             } else {
                 None
             };
-            info!(parent: &script_span, "executing build scripts");
-            for cmd in &self.recipe.build_script.steps {
+            for cmd in &config_script.steps {
                 if !cmd.images.is_empty() {
-                    trace!(parent: &script_span, images = ?cmd.images, "only execute on");
+                    trace!(images = ?cmd.images, "only execute on");
                     if !cmd.images.contains(&self.image.name) {
-                        trace!(parent: &script_span, image = %self.image.name, "not found, skipping");
+                        trace!(image = %self.image.name, "not found, skipping");
+                        continue;
+                    }
+                }
+                self.checked_exec(&cmd.cmd, working_dir, shell).await?;
+            }
+
+            Ok(())
+        }
+        .instrument(span)
+        .await
+    }
+
+    async fn run_build(&self, build_script: &BuildScript) -> Result<()> {
+        let span = info_span!("build");
+        async move {
+            let working_dir = if let Some(dir) = &build_script.working_dir {
+                Some(dir.as_path())
+            } else {
+                Some(self.container_bld_dir)
+            };
+            let shell = if let Some(shell) = &build_script.shell {
+                Some(shell.as_str())
+            } else {
+                None
+            };
+            info!("executing build scripts");
+            for cmd in &build_script.steps {
+                if !cmd.images.is_empty() {
+                    trace!(images = ?cmd.images, "only execute on");
+                    if !cmd.images.contains(&self.image.name) {
+                        trace!(image = %self.image.name, "not found, skipping");
                         continue;
                     }
                 }
 
-                self.checked_exec(&cmd.cmd, working_dir, shell).instrument(script_span.clone()).await?;
+                self.checked_exec(&cmd.cmd, working_dir, shell).await?;
             }
 
-            if let Some(install_script) = &self.recipe.install_script {
-                let script_span = info_span!("install");
-                let working_dir = if let Some(dir) = &install_script.working_dir {
-                    Some(dir.as_path())
-                } else {
-                    Some(self.container_out_dir)
-                };
-                let shell = if let Some(shell) = &install_script.shell {
-                    Some(shell.as_str())
-                } else {
-                    None
-                };
-                info!(parent: &script_span, "executing install scripts");
-                for cmd in &install_script.steps {
-                    if !cmd.images.is_empty() {
-                        trace!(parent: &script_span, images = ?cmd.images, "only execute on");
-                        if !cmd.images.contains(&self.image.name) {
-                            trace!(parent: &script_span, image = %self.image.name, "not found, skipping");
-                            continue;
-                        }
-                    }
+            Ok(())
+        }
+        .instrument(span)
+        .await
+    }
 
-                    self.checked_exec(&cmd.cmd, working_dir, shell).instrument(script_span.clone()).await?;
-                }
+    async fn run_install(&self, install_script: &InstallScript) -> Result<()> {
+        let span = info_span!("install");
+        async move {
+            let working_dir = if let Some(dir) = &install_script.working_dir {
+                Some(dir.as_path())
             } else {
-                info!("no install steps to run");
+                Some(self.container_out_dir)
+            };
+            let shell = if let Some(shell) = &install_script.shell {
+                Some(shell.as_str())
+            } else {
+                None
+            };
+            info!("executing install scripts");
+            for cmd in &install_script.steps {
+                if !cmd.images.is_empty() {
+                    trace!(images = ?cmd.images, "only execute on");
+                    if !cmd.images.contains(&self.image.name) {
+                        trace!(image = %self.image.name, "not found, skipping");
+                        continue;
+                    }
+                }
+
+                self.checked_exec(&cmd.cmd, working_dir, shell).await?;
             }
 
             Ok(())
