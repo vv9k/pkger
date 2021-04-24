cff1ef8dcf6637d7d9a7f00c326a26a36d33e19d
Wojciech Kępka <wojciech@wkepka.dev>
2021-04-24 07:17:58 +0200

    Cleanup some toml value parsing

--- src/deps.rs
+++ src/deps.rs
@@ -24,46 +24,62 @@ impl Default for Dependencies {
     }
 }
 
+impl TryFrom<toml::value::Table> for Dependencies {
+    type Error = crate::Error;
+
+    fn try_from(table: toml::value::Table) -> Result<Self, Self::Error> {
+        let mut deps = Self::default();
+        for (image, image_deps) in table {
+            if image_deps.is_array() {
+                let mut deps_set = HashSet::new();
+                for dep in image_deps.as_array().unwrap() {
+                    if !dep.is_str() {
+                        return Err(anyhow!(
+                            "expected a string as dependency, found `{:?}`",
+                            dep
+                        ));
+                    }
+
+                    deps_set.insert(dep.as_str().unwrap().to_string());
+                }
+                deps.inner_mut().insert(image.to_string(), deps_set);
+            } else {
+                return Err(anyhow!(
+                    "expected array of dependencies, found `{:?}`",
+                    image_deps
+                ));
+            }
+        }
+        Ok(deps)
+    }
+}
+
+impl TryFrom<toml::value::Array> for Dependencies {
+    type Error = crate::Error;
+    fn try_from(array: toml::value::Array) -> Result<Self> {
+        let mut deps = Self::default();
+        deps.inner_mut().insert(
+            COMMON_DEPS_KEY.to_string(),
+            array.iter().fold(HashSet::new(), |mut set, it| {
+                set.insert(it.to_string());
+                set
+            }),
+        );
+
+        Ok(deps)
+    }
+}
+
 impl TryFrom<toml::Value> for Dependencies {
     type Error = crate::Error;
     fn try_from(deps: toml::Value) -> Result<Self> {
-        if let toml::Value::Table(table) = deps {
-            let mut deps = Self::default();
-            for (image, image_deps) in table {
-                if image_deps.is_array() {
-                    deps.inner_mut().insert(
-                        image.to_string(),
-                        image_deps
-                            .as_array()
-                            .unwrap()
-                            .iter()
-                            .map(|v| v.to_string().trim_matches('"').to_string())
-                            .collect::<HashSet<_>>(),
-                    );
-                } else {
-                    return Err(anyhow!(
-                        "expected array of dependencies, found `{:?}`",
-                        image_deps
-                    ));
-                }
-            }
-            Ok(deps)
-        } else if let toml::Value::Array(array) = deps {
-            let mut deps = Self::default();
-            deps.inner_mut().insert(
-                COMMON_DEPS_KEY.to_string(),
-                array.iter().fold(HashSet::new(), |mut set, it| {
-                    set.insert(it.to_string());
-                    set
-                }),
-            );
-
-            Ok(deps)
-        } else {
-            Err(anyhow!(
+        match deps {
+            toml::Value::Table(table) => Self::try_from(table),
+            toml::Value::Array(array) => Self::try_from(array),
+            _ => Err(anyhow!(
                 "expected a map or array of dependencies, found `{:?}`",
                 deps
-            ))
+            )),
         }
     }
 }
