7cebb3ab4062bd5cf688fe9cbacd7b2f195f39a7
wojciechkepka <wojtek.kepka@protonmail.com>
2021-04-24 09:18:22 +0200

    Cleanup Cmd impl

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,4 +1,4 @@
-use crate::Result;
+use crate::{Error, Result};
 
 use std::convert::TryFrom;
 
@@ -15,46 +15,54 @@ pub struct Cmd {
     pub images: Vec<String>,
 }
 
-impl TryFrom<&toml::Value> for Cmd {
-    type Error = crate::Error;
+impl From<&str> for Cmd {
+    fn from(s: &str) -> Self {
+        Self {
+            cmd: s.to_string(),
+            images: vec![],
+        }
+    }
+}
 
-    fn try_from(value: &toml::Value) -> Result<Self> {
-        match value {
-            toml::Value::String(s) => Ok(Cmd {
-                cmd: s.to_string(),
+impl TryFrom<toml::value::Table> for Cmd {
+    type Error = Error;
+    fn try_from(table: toml::value::Table) -> Result<Self> {
+        if let Some(cmd) = table.get("cmd") {
+            let mut command = Cmd {
+                cmd: String::new(),
                 images: vec![],
-            }),
-            toml::Value::Table(table) => {
-                if let Some(cmd) = table.get("cmd") {
-                    let mut command = Cmd {
-                        cmd: String::new(),
-                        images: vec![],
-                    };
-                    if !cmd.is_str() {
-                        return Err(anyhow!("expected a string as command, found `{:?}`", cmd));
-                    }
-                    command.cmd = cmd.as_str().unwrap().to_string();
-                    if let Some(images) = table.get("images") {
-                        if !images.is_array() {
-                            return Err(anyhow!("expected an array of images, found `{:?}`", cmd));
-                        }
+            };
+            if !cmd.is_str() {
+                return Err(anyhow!("expected a string as command, found `{:?}`", cmd));
+            }
+            command.cmd = cmd.as_str().unwrap().to_string();
+            if let Some(images) = table.get("images") {
+                if !images.is_array() {
+                    return Err(anyhow!("expected an array of images, found `{:?}`", cmd));
+                }
 
-                        for image in images.as_array().unwrap() {
-                            if !image.is_str() {
-                                return Err(anyhow!(
-                                    "expected a string as image, found `{:?}`",
-                                    cmd
-                                ));
-                            }
-                            command.images.push(image.as_str().unwrap().to_string())
-                        }
+                for image in images.as_array().unwrap() {
+                    if !image.is_str() {
+                        return Err(anyhow!("expected a string as image, found `{:?}`", cmd));
                     }
-
-                    Ok(command)
-                } else {
-                    Err(anyhow!("expected `cmd` key"))
+                    command.images.push(image.as_str().unwrap().to_string())
                 }
             }
+
+            Ok(command)
+        } else {
+            Err(anyhow!("expected `cmd` key"))
+        }
+    }
+}
+
+impl TryFrom<toml::Value> for Cmd {
+    type Error = crate::Error;
+
+    fn try_from(value: toml::Value) -> Result<Self> {
+        match value {
+            toml::Value::String(s) => Ok(Cmd::from(s.as_str())),
+            toml::Value::Table(table) => Self::try_from(table),
             val => Err(anyhow!(
                 "expected a table or a string as command, found `{:?}`",
                 val
